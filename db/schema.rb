800387eea83059ea508698bda80aaca2b291a403
Casper <charles.opokuagyemang@casper.com>
2022-02-19 08:44:01 +0000

    afa

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_065738) do
+ActiveRecord::Schema.define(version: 2022_02_19_084216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,13 +113,6 @@ ActiveRecord::Schema.define(version: 2022_02_18_065738) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["contribution_id"], name: "index_payouts_on_contribution_id"
     t.index ["investor_id"], name: "index_payouts_on_investor_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
