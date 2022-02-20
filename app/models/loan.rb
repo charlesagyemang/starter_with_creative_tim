388743237b05414bf221ca53e5906c58b3513e5f
Casper <charles.opokuagyemang@casper.com>
2022-02-20 17:50:05 +0000

    kksks

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -6,5 +6,4 @@ class Loan < ApplicationRecord
   def set_amount
     self.amount = self.interest_on_loan_per_month > 0.00 ? (((self.principal * self.interest_on_loan_per_month) / 100) *  self.principal) + self.loan_period_in_months : self.principal
   end
-
 end
