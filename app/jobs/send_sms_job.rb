326acf0caeda3e3b89bfced150e121ff1e6e1ea3
Casper <charles.opokuagyemang@casper.com>
2022-02-20 06:54:33 +0000

    Anumils

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -4,19 +4,20 @@ class SendSmsJob < ApplicationJob
   def perform(loan_payment)
     # send_sms(to, message)
     phone_number = loan_payment.loaner.phone_number
-    total_payment = LoanPayment.where(loan_id: loan_payment.loan_id).sum(:amount)
+    total_payment = LoanPayment.where(loan_id: loan_payment.loan_id, loaner_id: loan_payment.loaner_id).sum(&:amount).to_i
     message = "Amount Received: #{loan_payment.amount}.\n
-    Mode Of Payment: #{loan_payment.mode}.\n
-    Total Payment: #{loan_payment.next_payment_date}\n
+    Mode Of Payment: #{loan_payment.mode}\n
+    Total Payment: #{total_payment}\n
     Next Payment Date: #{loan_payment.next_payment_date}\n
-    Amount Remaning: #{loan_payment.amount - total_payment}
-    "
+    Amount Remaning: #{loan_payment.loan.amount - total_payment}"
+    puts "============ MESSAGES #{message} ====================="
+    send_sms(phone_number, message)
   end
 
   private
     def send_sms(to, message)
       client = WITTLY_FLOW_CLIENT
-      res = client.send_sms("A.G.B.A", to, message)
+      res = client.send_sms("BenjiCap", to, message)
       puts "============ SENDING SMS TO #{to} SMS LOGS: #{message} ====================="
       puts "============ SMS LOGS: #{res} ====================="
     end
--- app/models/loan_payment.rb
+++ app/models/loan_payment.rb
@@ -1,4 +1,16 @@
 class LoanPayment < ApplicationRecord
   belongs_to :loaner
   belongs_to :loan
+
+  after_create :set_next_payment_date
+
+  def set_next_payment_date
+    next_week_date = Date.today.next_occurring(self.loan.payment_day.downcase.to_sym)
+    cadence = {
+      'WEEKLY' => -> { next_week_date },
+      'BI_WEEKLY' => -> { next_week_date + 1.week},
+      'MONTHLY' => -> { next_week_date + 3.week}
+    }
+    self.next_payment_date = cadence[self.loan.payment_cadence]
+  end
 end
