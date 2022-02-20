0d931fd7f100bba0577c528072c5103d27a3b438
Casper <charles.opokuagyemang@casper.com>
2022-02-20 07:34:15 +0000

    amoka siase

--- app/jobs/send_sms_job.rb
+++ app/jobs/send_sms_job.rb
@@ -5,11 +5,7 @@ class SendSmsJob < ApplicationJob
     # send_sms(to, message)
     phone_number = loan_payment.loaner.phone_number
     total_payment = LoanPayment.where(loan_id: loan_payment.loan_id, loaner_id: loan_payment.loaner_id).map { |x| x.amount.to_i }.inject(0, :+)
-    message = "Amount Received: #{loan_payment.amount}.\n
-    Mode Of Payment: #{loan_payment.mode}\n
-    Total Payment: #{total_payment}\n
-    Next Payment Date: #{loan_payment.next_payment_date.strftime("%B %d, %Y")}\n
-    Amount Remaning: #{loan_payment.loan.amount - total_payment}"
+    message = "Hi #{loan_payment.loaner.first_name}, we have received #{loan_payment.amount} from you via #{loan_payment.mode}. check your stats below\nTotal Paid: #{total_payment}\nNext Payment Date: #{loan_payment.next_payment_date.strftime("%B %d, %Y")}\nAmount Remaning: #{loan_payment.loan.amount - total_payment}"
     puts "============ MESSAGES #{message} ====================="
     send_sms(phone_number, message)
   end
